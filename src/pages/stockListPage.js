2164d47e1e7bba97377ba72df948f91392519de3
Kepha Kibet <kibet.kepha@live.com>
2022-10-12 18:08:38 +0300

    set up stock list and autocomplete components

--- src/pages/stockListPage.js
+++ src/pages/stockListPage.js
@@ -1,9 +1,11 @@
-import React from "react"
-
+import { AutoComplete } from "../components/autoComplete"
+import { StockList } from "../components/stockList"
 
 export const StockListPage = () => {
   
   return <div>
-      This is the Stock List Page
+    This is the Stock List Page
+    <AutoComplete />
+    <StockList />
   </div>
 }
