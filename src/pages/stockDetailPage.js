6f2642791d8347c5e0c3fd1699d1e2fae0144a3b
Kepha Kibet <kibet.kepha@live.com>
2022-10-20 08:59:02 +0300

    get historical data including weekends when stock market is closed

--- src/pages/stockDetailPage.js
+++ src/pages/stockDetailPage.js
@@ -1,8 +1,42 @@
 import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import axios from "axios"
+import finnHub from "../api/finnHub"
 
 export const StockDetailPage = () => {
 
-  const {symbol}=useParams()
+  const { symbol } = useParams()
+  useEffect(() => {
+    const fetchData = async () => {
+      const date = new Date()
+      // convert to seconds and truncate.
+      const currentTime = Math.floor(date.getTime() / 1000)
+      let oneDay 
+      if (date.getDay() === 6) {
+        oneDay = currentTime - 2 * 24 * 60 * 60;
+      }
+      else if (date.getDay() === 0) {
+        oneDay = currentTime - 3 * 24 * 60 * 60;
+      }
+      else {
+        oneDay = currentTime - 24 * 60 * 60;
+      }
+
+
+
+      const response = await finnHub.get("/stock/candle", {
+        params: {
+          symbol,
+          from:oneDay,
+          to:currentTime,
+          resolution:30
+        }
+      })
+      console.log(response);
+    }
+    fetchData()
+  }, [])
+  
   
   return <div>
       {symbol}
